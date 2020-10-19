1fe26a3106c5cb5212801c3f167556cd2094abea
Adam <surfcoderepeat@protonmail.com>
2020-10-18 19:23:03 -0600

    added soon dead, will clean up code later

--- index.js
+++ index.js
@@ -1,65 +1,73 @@
-require('dotenv').config()
+require("dotenv").config();
 const Telegraf = require("telegraf");
+const extra = require("telegraf/extra");
+const markup = extra.markdown();
+
 const bot = new Telegraf(process.env.TELEGRAM_KEY);
-const dayjs = require('dayjs')
-const relativeTime = require('dayjs/plugin/relativeTime')
-dayjs.extend(relativeTime)
+const dayjs = require("dayjs");
+const relativeTime = require("dayjs/plugin/relativeTime");
+dayjs.extend(relativeTime);
 const { ethers } = require("ethers");
-const BigNumber = require('bignumber.js');
+const BigNumber = require("bignumber.js");
 
-const VNFTAbi = require("./VNFT.abi")
-const VNFTAddress = require("./VNFT.address")
+const VNFTAbi = require("./VNFT.abi");
+const VNFTAddress = require("./VNFT.address");
 
-
-var provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${process.env.INFURA}`);
+var provider = new ethers.providers.JsonRpcProvider(
+  `https://mainnet.infura.io/v3/${process.env.INFURA}`
+);
 const vnftContract = new ethers.Contract(VNFTAddress, VNFTAbi, provider);
 
+const soon = require("./soon.js");
 
-bot.command("vnft", async ctx => {
-    try {
+bot.command("vnft", async (ctx) => {
+  try {
+    let msg = ctx.update.message.text;
 
-        let msg = ctx.update.message.text;
+    const vnftId = parseInt(msg.replace("/vnft", ""));
 
-        const vnftId = parseInt(msg.replace("/vnft", ""));
+    const vnftDetails = await vnftContract.getVnftInfo(vnftId);
 
+    const nftId = vnftDetails[0].toNumber();
+    const isAlive = vnftDetails[1];
+    const score = vnftDetails[2].toNumber();
+    const level = vnftDetails[3].toNumber();
+    let expectedReward = BigNumber(vnftDetails[4]._hex);
+    expectedReward = expectedReward.shiftedBy(-18).toFixed(2);
+    const timeUntilStarving = vnftDetails[5].toNumber();
+    const lastTimeMined = vnftDetails[6].toNumber();
+    const timeVnftBorn = vnftDetails[7].toNumber();
+    const owner = vnftDetails[8];
+    const originAddress = vnftDetails[9];
+    const originTokenId = vnftDetails[10].toNumber();
 
-        const vnftDetails = await vnftContract.getVnftInfo(vnftId)
+    const starvingAt = dayjs(timeUntilStarving * 1000);
+    const todHrsLeft = starvingAt.diff(dayjs(), "hours");
 
-        const nftId = vnftDetails[0].toNumber()
-        const isAlive = vnftDetails[1]
-        const score = vnftDetails[2].toNumber()
-        const level = vnftDetails[3].toNumber()
-        let expectedReward = BigNumber(vnftDetails[4]._hex);
-        expectedReward = expectedReward.shiftedBy(-18).toFixed(2);
-        const timeUntilStarving = vnftDetails[5].toNumber()
-        const lastTimeMined = vnftDetails[6].toNumber()
-        const timeVnftBorn = vnftDetails[7].toNumber()
-        const owner = vnftDetails[8]
-        const originAddress = vnftDetails[9]
-        const originTokenId = vnftDetails[10].toNumber();
+    const tod = isAlive ? `TOD: ${todHrsLeft} hrs` : `TOD: dead`;
 
-
-        const starvingAt = dayjs(timeUntilStarving * 1000);
-        const todHrsLeft = starvingAt.diff(dayjs(), "hours");
-        
-        const tod = isAlive ? `TOD: ${todHrsLeft} hrs` : `TOD: dead`;
-
-        
-        await ctx.reply(`ID: #${nftId} \n` +
-                        `${tod} \n` +
-                        `level: ${level} \n` +
-                        `Last Mined: ${dayjs(lastTimeMined * 1000).format("D-M HH:mm")} \n`+ 
-                        `Score: ${score} \n` +
-                        `Current reward: ${expectedReward}muse \n`
-        )
-
-    } catch(e) {
-        console.log(e)
-        ctx.reply("Wrong command")
-    }
-
+    await ctx.reply(
+      `ID: #${nftId} \n` +
+        `${tod} \n` +
+        `level: ${level} \n` +
+        `Last Mined: ${dayjs(lastTimeMined * 1000).format("D-M HH:mm")} \n` +
+        `Score: ${score} \n` +
+        `Current reward: ${expectedReward}muse \n`
+    );
+  } catch (e) {
+    console.log(e);
+    ctx.reply("Wrong command");
+  }
 });
 
+bot.command("fatality", async (ctx) => {
+  ctx.reply("Getting data...");
 
+  const msgs = await soon();
+  console.log(msgs);
+  for (msg of msgs) {
+    ctx.reply(msg, markup);
+  }
+});
 
 bot.launch();
